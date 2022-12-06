8383b7504c0ead3fcaedc9558fe5c300d561d4e8
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-06 01:34:01 +0100

    1 file modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,19 +12,23 @@ describe('AppComponent', () => {
   // Arrange phase (setup)
   beforeEach(async () => {
     helloServiceSpy = jasmine.createSpyObj('HelloService', ['getHelloWorld']);
-    helloServiceSpy.getHelloWorld.and.returnValue('mocked hello');
+    helloServiceSpy.getHelloWorld.and.returnValue('Mocked hello !');
 
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       providers: [{ provide: HelloService, useValue: helloServiceSpy }],
     }).compileComponents();
 
+    // Create the component fixture that allows to inspect the component
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('#getHelloWorld should return real value from the real service', () => {
     expect(fixture.componentInstance.helloService.getHelloWorld()).toBe(
-      'mocked hello'
+      'Mocked hello !'
     );
   });
 });
